1caa9b56769528ca11305cef0bc60d2800ad4037
ChrisEskridge <Chris@ChrisEskridge.com>
2018-06-19 10:31:35 -0400

    Update 404.php

--- 404.php
+++ 404.php
@@ -9,29 +9,33 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'outdoorfirepits' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'outdoorfirepits' ); ?></p>
-
-					<?php
+<div id="primary" class="content-area">
+  <main id="main" class="site-main" role="main">
+    <section class="error-404 not-found">
+      <header class="page-header">
+        <h1 class="page-title">
+          <?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'outdoorfirepits' ); ?>
+        </h1>
+      </header>
+      <!-- .page-header -->
+      
+      <div class="page-content">
+        <p>
+          <?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'outdoorfirepits' ); ?>
+        </p>
+        <?php
 						get_search_form();
 
 						the_widget( 'WP_Widget_Recent_Posts' );
 
 						if ( outdoorfirepits_categorized_blog() ) : // Only show the widget if site has multiple categories.
 					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'outdoorfirepits' ); ?></h2>
-						<ul>
-						<?php
+        <div class="widget widget_categories">
+          <h2 class="widget-title">
+            <?php esc_html_e( 'Most Used Categories', 'outdoorfirepits' ); ?>
+          </h2>
+          <ul>
+            <?php
 							wp_list_categories( array(
 								'orderby'    => 'count',
 								'order'      => 'DESC',
@@ -40,10 +44,11 @@ get_header(); ?>
 								'number'     => 10,
 							) );
 						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
+          </ul>
+        </div>
+        <!-- .widget -->
+        
+        <?php
 						endif;
 
 						/* translators: %1$s: smiley */
@@ -52,12 +57,16 @@ get_header(); ?>
 
 						the_widget( 'WP_Widget_Tag_Cloud' );
 					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+      </div>
+      <!-- .page-content --> 
+    </section>
+    <!-- .error-404 --> 
+    
+  </main>
+  <!-- #main --> 
+</div>
+<!-- #primary -->
 
 <?php
 get_footer();
+
